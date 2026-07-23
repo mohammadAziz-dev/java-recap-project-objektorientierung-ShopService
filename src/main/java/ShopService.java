50d41ff91c9e43f6420bf9b6e8932dcd4a8ee4a2
Mohammad Abdul Aziz <m.a.aziz1900@gmail.com>
2026-07-23 13:12:42 +0200

    Add order status to orders

--- src/main/java/ShopService.java
+++ src/main/java/ShopService.java
@@ -11,13 +11,14 @@ public class ShopService {
         for (String productId : productIds) {
             Product productToOrder = productRepo.getProductById(productId);
             if (productToOrder == null) {
-                System.out.println("Product mit der Id: " + productId + " konnte nicht bestellt werden!");
+                System.out.println("Product with ID " + productId
+                        + " could not be ordered.");
                 return null;
             }
             products.add(productToOrder);
         }
 
-        Order newOrder = new Order(UUID.randomUUID().toString(), products);
+        Order newOrder = new Order(UUID.randomUUID().toString(), products, OrderStatus.PROCESSING);
 
         return orderRepo.addOrder(newOrder);
     }
